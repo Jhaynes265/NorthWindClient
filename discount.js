ab0b068cedfaae7644958596d42f5f1e7b6a97e5
Jared Haynes <jhaynes4@my.wctc.edu>
2026-01-31 13:12:27 -0600

    {2.19} Toast shows code and title with data- attributes

--- discount.js
+++ discount.js
@@ -4,9 +4,13 @@ document.addEventListener("DOMContentLoaded", function() {
     if (e.target.classList.contains('discount')) {
       e.preventDefault();
             document.getElementById('product').innerHTML = e.target.dataset['product'];
+            document.getElementById('title').innerHTML = e.target.dataset['title'];
+            document.getElementById('code').innerHTML = e.target.dataset['code'];
       bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
     }
     });
+
+    
 
     const toastID = document.getElementById('liveToast');
     const toast = bootstrap.Toast.getOrCreateInstance(toastID);
